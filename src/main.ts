48849911ac5d98c1045f171e58f346c0fa0526f4
JD Cargile <jcargile@control4.com>
2020-05-07 23:34:36 -0600

    prod dependencies

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,6 @@ async function run(): Promise<void> {
     const ms: string = core.getInput('milliseconds')
     core.debug(`Waiting ${ms} milliseconds ...`)
 
-	const webhookUri: string = core.getInput("ms-teams-webhook-uri", { required: true });
-	const notificationMessage = core.getInput("message") || "GitHub Action";
-
     core.debug(new Date().toTimeString())
     await wait(parseInt(ms, 10))
     core.debug(new Date().toTimeString())
