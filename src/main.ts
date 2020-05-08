3dd714e3487bf5a3695b07430f08694f62941cfc
JD Cargile <jcargile@control4.com>
2020-05-07 23:54:20 -0600

    Add MS teams call
    Update descriptions and inputs
    Added axios dependency

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,24 @@
 import * as core from '@actions/core';
 import { wait } from './wait';
+const axios = require('axios').default;
 
 async function run(): Promise<void> {
   try {
     const ms: string = core.getInput('milliseconds')
     core.debug(`Waiting ${ms} milliseconds ...`)
+
+	const msTeamsWebhookUri: string = core.getInput("ms-teams-webhook-uri", { required: true });
+	const notificationMessage = core.getInput("message") || "GitHub Action";
+
+	axios.post(msTeamsWebhookUri, {
+		body: notificationMessage
+	})
+	.then(function (response: any) {
+		console.log(response);
+	})
+	.catch(function (error: any) {
+		console.log(error);
+	});
 
     core.debug(new Date().toTimeString())
     await wait(parseInt(ms, 10))
