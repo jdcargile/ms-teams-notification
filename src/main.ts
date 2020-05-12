82b28834cb406a4b3b265c8e9c3634839721a4c0
JD Cargile <jcargile@control4.com>
2020-05-11 22:45:47 -0600

    Initial Commit

---
+++ src/main.ts
@@ -0,0 +1,75 @@
+import * as core from '@actions/core'
+import {Octokit} from '@octokit/rest'
+import axios from 'axios'
+import moment from 'moment-timezone'
+import {createMessageCard} from './message-card'
+
+const escapeMarkdownTokens = (text: string) =>
+  text
+    .replace(/\n\ {1,}/g, '\n ')
+    .replace(/\_/g, '\\_')
+    .replace(/\*/g, '\\*')
+    .replace(/\|/g, '\\|')
+    .replace(/#/g, '\\#')
+    .replace(/-/g, '\\-')
+    .replace(/>/g, '\\>')
+
+async function run(): Promise<void> {
+  try {
+    const githubToken = core.getInput('github-token', {required: true})
+    const msTeamsWebhookUri: string = core.getInput('ms-teams-webhook-uri', {
+      required: true
+    })
+
+    const notificationSummary =
+      core.getInput('notification-summary') || 'GitHub Action Notification'
+    const notificationColor = core.getInput('notification-color') || '0b93ff'
+    const timezone = core.getInput('timezone') || 'UTC'
+
+    const timestamp = moment()
+      .tz(timezone)
+      .format('dddd, MMMM Do YYYY, h:mm:ss a z')
+
+    const [owner, repo] = (process.env.GITHUB_REPOSITORY || '').split('/')
+    const sha = process.env.GITHUB_SHA || ''
+    const runId = process.env.GITHUB_RUN_ID || ''
+    const runNum = process.env.GITHUB_RUN_NUMBER || ''
+    const params = {owner, repo, ref: sha}
+    const repoName = params.owner + '/' + params.repo
+    const repoUrl = `https://github.com/${repoName}`
+
+    const octokit = new Octokit({auth: `token ${githubToken}`})
+    const commit = await octokit.repos.getCommit(params)
+    const author = commit.data.author
+
+    const messageCard = await createMessageCard(
+      notificationSummary,
+      notificationColor,
+      commit,
+      author,
+      runNum,
+      runId,
+      repoName,
+      sha,
+      repoUrl,
+      timestamp
+    )
+
+    console.log(messageCard)
+
+    axios
+      .post(msTeamsWebhookUri, messageCard)
+      .then(function(response) {
+        console.log(response)
+        core.debug(response.data)
+      })
+      .catch(function(error) {
+        core.debug(error)
+      })
+  } catch (error) {
+    console.log(error)
+    core.setFailed(error.message)
+  }
+}
+
+run()
---
+++ src/message-card.ts
@@ -0,0 +1,45 @@
+export function createMessageCard(
+  notificationSummary: string,
+  notificationColor: string,
+  commit: any,
+  author: any,
+  runNum: string,
+  runId: string,
+  repoName: string,
+  sha: string,
+  repoUrl: string,
+  timestamp: string
+): any {
+  const messageCard = {
+    '@type': 'MessageCard',
+    '@context': 'https://schema.org/extensions',
+    summary: notificationSummary,
+    themeColor: notificationColor,
+    title: notificationSummary,
+    sections: [
+      {
+        activityTitle: `**CI #${runNum} (commit ${sha.substr(
+          0,
+          7
+        )})** on [${repoName}](${repoUrl})`,
+        activityImage: author.avatar_url,
+        activitySubtitle: `by ${commit.data.commit.author.name} [(@${author.login})](${author.html_url}) on ${timestamp}`
+      }
+    ],
+    potentialAction: [
+      {
+        '@context': 'http://schema.org',
+        target: [`${repoUrl}/actions/runs/${runId}`],
+        '@type': 'ViewAction',
+        name: 'View Workflow Run'
+      },
+      {
+        '@context': 'http://schema.org',
+        target: [commit.data.html_url],
+        '@type': 'ViewAction',
+        name: 'View Commit Changes'
+      }
+    ]
+  }
+  return messageCard
+}
