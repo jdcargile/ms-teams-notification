803b93a1aff64727fc6eb4e7c1594dd0af814def
DenisaHalmaghi <denisa.halmaghi@graffino.com>
2023-07-24 17:40:34 +0300

    test

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,15 @@ import axios from 'axios'
 import moment from 'moment-timezone'
 import {createMessageCard} from './message-card'
 
+enum JobStatus {
+  FAILURE = 'failure',
+  SUCCESS = 'success'
+}
+
+type Job = {
+  status: JobStatus
+}
+
 const escapeMarkdownTokens = (text: string) =>
   text
     .replace(/\n\ {1,}/g, '\n ')
@@ -14,6 +23,21 @@ const escapeMarkdownTokens = (text: string) =>
     .replace(/-/g, '\\-')
     .replace(/>/g, '\\>')
 
+const basicConfig = {
+  success: {
+    title: 'Workflow succedeed &#x1F680',
+    color: '82f071'
+  },
+  failure: {
+    title: 'Workflow failed &#x1F635',
+    color: 'd91633'
+  },
+  noStatus: {
+    color: '0b93ff',
+    title: 'GitHub Action Notification'
+  }
+}
+
 async function run(): Promise<void> {
   try {
     const githubToken = core.getInput('github-token', {required: true})
@@ -21,9 +45,23 @@ async function run(): Promise<void> {
       required: true
     })
 
+    let defaultConfig = basicConfig.noStatus
+
+    try {
+      const job = JSON.parse(core.getInput('job')) as Job
+
+      defaultConfig =
+        job.status == JobStatus.FAILURE
+          ? basicConfig.failure
+          : basicConfig.success
+    } catch (error) {
+      console.log('Job was not provided')
+    }
+
     const notificationSummary =
-      core.getInput('notification-summary') || 'GitHub Action Notification'
-    const notificationColor = core.getInput('notification-color') || '0b93ff'
+      core.getInput('notification-summary') || defaultConfig.title
+    const notificationColor =
+      core.getInput('notification-color') || defaultConfig.color
     const timezone = core.getInput('timezone') || 'UTC'
 
     const timestamp = moment()
