f82c88bb9c90862b46f39ab0ba4529bf01c5569c
JD Cargile <jdcargile@gmail.com>
2024-02-21 11:45:12 -0700

    fix: Evaluate verbose-logging as a bool

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,8 @@ async function run(): Promise<void> {
       core.getInput('notification-summary') || 'GitHub Action Notification'
     const notificationColor = core.getInput('notification-color') || '0b93ff'
     const timezone = core.getInput('timezone') || 'UTC'
-    const verboseLogging = core.getInput('verbose-logging')
+    const verboseLogging =
+      core.getInput('verbose-logging').toLowerCase() === 'true'
     const timestamp = moment()
       .tz(timezone)
       .format('dddd, MMMM Do YYYY, h:mm:ss a z')
